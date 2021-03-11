2c41b769333218f9a014e7cdf5b1302c4b93d16c
BillTestGroup <billtestgroup@gmail.com>
2021-03-11 14:06:40 +0300

    add new test for instalment 24

---
+++ test_buyTANaSim24.py
@@ -0,0 +1,29 @@
+import time
+import pytest
+from src import hybris_operations as HB, ma_dudes as dudes
+
+test_dude = dudes.Private_dude
+vix_creds = dudes.VIX_credentials
+wso = dudes.WSO_prod
+login = test_dude.login
+passw = test_dude.password
+
+#fixture
+@pytest.fixture
+def br():
+    site = 'https://www.a1.by/ru/'
+    br = HB.open_browser(site)
+    yield br
+    br.save_screenshot("./screenshots/"+(str(time.strftime('%Y-%m-%d-%H-%M'))) + ".png")
+    br.quit()
+
+
+def test_buyTANaSim24(br):
+    HB.login_site(br, login, passw)
+    HB.check_cart(br)
+    HB.cut_pop_up(br)
+    installment_price = HB.buyTANaSim24(br, test_dude)
+    HB.wait_for_order_in_work(br)
+    time.sleep(20)
+    HB.log_in_wso(br, wso, vix_creds)
+    HB.check_wso_installment(br, test_dude, installment_price)
